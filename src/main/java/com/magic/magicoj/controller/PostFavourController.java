6f771cd751a62893fd4bfe4aaa7107787251729a
magic-monster <magic.bozhang@outlook.com>
2024-05-28 08:08:33 +0800

    第一次提交：后端项目初始化

---
+++ src/main/java/com/magic/magicoj/controller/PostFavourController.java
@@ -0,0 +1,106 @@
+package com.magic.magicoj.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.magic.magicoj.common.BaseResponse;
+import com.magic.magicoj.common.ErrorCode;
+import com.magic.magicoj.common.ResultUtils;
+import com.magic.magicoj.exception.BusinessException;
+import com.magic.magicoj.exception.ThrowUtils;
+import com.magic.magicoj.model.dto.post.PostQueryRequest;
+import com.magic.magicoj.model.dto.postfavour.PostFavourAddRequest;
+import com.magic.magicoj.model.dto.postfavour.PostFavourQueryRequest;
+import com.magic.magicoj.model.entity.Post;
+import com.magic.magicoj.model.entity.User;
+import com.magic.magicoj.model.vo.PostVO;
+import com.magic.magicoj.service.PostFavourService;
+import com.magic.magicoj.service.PostService;
+import com.magic.magicoj.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子收藏接口
+ */
+@RestController
+@RequestMapping("/post_favour")
+@Slf4j
+public class PostFavourController {
+
+    @Resource
+    private PostFavourService postFavourService;
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 收藏 / 取消收藏
+     *
+     * @param postFavourAddRequest
+     * @param request
+     * @return resultNum 收藏变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doPostFavour(@RequestBody PostFavourAddRequest postFavourAddRequest,
+            HttpServletRequest request) {
+        if (postFavourAddRequest == null || postFavourAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能操作
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postFavourAddRequest.getPostId();
+        int result = postFavourService.doPostFavour(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 获取我收藏的帖子列表
+     *
+     * @param postQueryRequest
+     * @param request
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<PostVO>> listMyFavourPostByPage(@RequestBody PostQueryRequest postQueryRequest,
+            HttpServletRequest request) {
+        if (postQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long current = postQueryRequest.getCurrent();
+        long size = postQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postQueryRequest), loginUser.getId());
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+
+    /**
+     * 获取用户收藏的帖子列表
+     *
+     * @param postFavourQueryRequest
+     * @param request
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<PostVO>> listFavourPostByPage(@RequestBody PostFavourQueryRequest postFavourQueryRequest,
+            HttpServletRequest request) {
+        if (postFavourQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        long current = postFavourQueryRequest.getCurrent();
+        long size = postFavourQueryRequest.getPageSize();
+        Long userId = postFavourQueryRequest.getUserId();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20 || userId == null, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postFavourQueryRequest.getPostQueryRequest()), userId);
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+}
---
+++ src/main/java/com/magic/magicoj/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.magic.magicoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.magic.magicoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
